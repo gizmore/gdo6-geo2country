85bedd18a336274962241b3536c816582d6cc16e
gizmore <gizmore@wechall.net>
2019-08-31 16:26:32 +0200

    6.10-r42_german_lang

---
+++ lang/geo2country_de.php
@@ -0,0 +1,20 @@
+<?php
+return array(
+'link_geo2ctry_try_api' => 'API ausprobieren',
+'btn_your_location' => 'Versuchen den eigenen Standort zu ermitteln',
+'pick_position' => 'Standort auswählen',
+'btn_picked_location' => 'Versuchen Sie den ausgewählten Standort',
+'geoapi_link_example' => 'Beispiel API Aufruf',
+'geoapi_info_title' => 'Geo2Country API',
+'geoapi_info_text' => '
+Dies ist die Testseite der Geo2Country API.<br/>
+Klicken Sie %s für einen Beispiel-Aufruf, welcher in &quot;de&quot; resultieren sollte.<br/>
+Sie können auch Ihr Land durch Ihre eigene Position bestimmen lassen, sowie einen Punkt auf der Karte auswählen.
+',
+'geoapi_coming_soon' => '
+<b>Geplant</b>: Länderinformationen wie Sprachen, Einwohnerzahl, Währungen etc. zur Verfügung stellen.<br/>
+<br/>
+<b>Wie es funktioniert</b>: Mittels Daten von https://github.com/mledoze/countries und Beschleunigung durch umschliessende Rechtecke.<br/>
+&nbsp;&nbsp;&nbsp;Die möglichen Länder werden dann auf Polygone geprüft.
+',
+);
